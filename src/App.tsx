e77269612b549ba96187a491436e6401487a1d8e
marksv21 <marvmms@gmail.com>
2023-06-19 12:44:48 -0300

    feact: termino do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prefer-const */
 import { useEffect, useState } from 'react'
 import * as C from './App.styles';
 import logoImage from './assets/devmemory_logo.png'
@@ -7,22 +9,68 @@ import { InfoItem } from './components/InfoItem';
 import { GridItemType } from './types/GridItemType';
 import { itens } from './data/items'
 import { GridItem } from './components/GridItem';
+import { formatTimeElapsed } from './helpers/formatTimeElapsed';
 
 const App = () => {
   const [playing, Setplaying] = useState<boolean>(false);
-  const [timeElapsed, SetTimeElapsed] = useState<number>(0);
-  const [moveCount, SetMoveCount] = useState<number>(0);
-  const [showCount, SetShowCount] = useState<number>(0);
-  const [gridItems, SetGridItems] = useState<GridItemType[]>([]);
+  const [timeElapsed, setTimeElapsed] = useState<number>(0);
+  const [moveCount, setMoveCount] = useState<number>(0);
+  const [shownCount, setShownCount] = useState<number>(0);
+  const [gridItems, setGridItems] = useState<GridItemType[]>([]);
 
 
   useEffect(()=> resetAndCreateGrid(), []);
 
+  useEffect(()=> {
+    const timer = setInterval(()=>{
+      if(playing){
+        setTimeElapsed(timeElapsed + 1)
+      }
+    },1000);
+    return ()=> clearInterval(timer)
+  }, [playing, timeElapsed])
+
+  useEffect(()=> {
+    if(shownCount === 2){
+      let opened = gridItems.filter(item => item.shown === true);
+      if(opened.length === 2){
+
+        if(opened[0].item === opened[1].item){
+          let tmpGrid = [...gridItems];
+          for(let i in tmpGrid){
+            if(tmpGrid[i].shown){
+              tmpGrid[i].permanentShown = true;
+              tmpGrid[i].shown = false;
+            }
+          }
+          setGridItems(tmpGrid);
+          setShownCount(0)
+        } else{
+          setTimeout(() => {
+            let tmpGrid = [...gridItems];
+            for(let i in tmpGrid){
+            tmpGrid[i].shown = false;
+          }
+          setGridItems(tmpGrid);
+          setShownCount(0)
+          }, 1000);
+        }
+        setMoveCount(moveCount => moveCount + 1);
+      }
+    }
+  },[shownCount, gridItems])
+
+  useEffect(()=>{
+    if(moveCount > 0 && gridItems.every(item => item.permanentShown === true)){
+      Setplaying(false);
+    }
+  },[moveCount, gridItems])
+
   const resetAndCreateGrid = () => {
     // passo 1 resetar o jogo
-    SetTimeElapsed(0);
-    SetMoveCount(0);
-    SetShowCount(0);
+    setTimeElapsed(0);
+    setMoveCount(0);
+    setShownCount(0);
 
     // passo 2 - criar o grid
     // 2.1 - criar um grid vazio
@@ -41,15 +89,22 @@ const App = () => {
       }
     }
     //2.3 - jogar no state
-    SetGridItems(tmpGrid);
+    setGridItems(tmpGrid);
     
     // passo 3 - começar o jogo
     Setplaying(true);
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
   const handleItemClick = (index:number) => {
-    
+    if(playing && index !== null && shownCount < 2){
+      let tmpGrid = [...gridItems];
+
+      if(tmpGrid[index].permanentShown === false && tmpGrid[index].shown ===false){
+        tmpGrid[index].shown = true;
+        setShownCount(shownCount + 1);
+      }
+      setGridItems(tmpGrid);
+    }
   }
 
   return (
@@ -60,8 +115,8 @@ const App = () => {
         </C.LogoLink>
 
         <C.InfoArea>
-          <InfoItem label='Tempo' value='00:00'/>
-          <InfoItem label='Movimentos' value='0'/>
+          <InfoItem label='Tempo' value={ formatTimeElapsed(timeElapsed) }/>
+          <InfoItem label='Movimentos' value={moveCount.toString()}/>
         </C.InfoArea>
 
         <Button label='Reniciar' icon={RestartIcon} onClick={resetAndCreateGrid}/>
